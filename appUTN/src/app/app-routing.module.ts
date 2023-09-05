d5aba432e6f704b575b74437af49280ad331a115
fabellaivan <ivano.fabella@gmail.com>
2023-09-04 21:07:31 -0300

    cambio

--- appUTN/src/app/app-routing.module.ts
+++ appUTN/src/app/app-routing.module.ts
@@ -1,15 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { BienvenidoComponent } from './bienvenido/bienvenido.component';
-import { LoginComponent } from './login/login.component';
-import { ErrorComponent } from './error/error.component';
-
-const routes: Routes = [
-  { path: '', component: BienvenidoComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'error', component: ErrorComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
